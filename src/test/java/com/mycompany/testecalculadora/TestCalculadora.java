97633cd60dda773448d06346dfc12f01a540ff48
Guilherme Schveitzer <164898300+GuilhermeSchveitzer@users.noreply.github.com>
2025-04-15 10:42:34 -0300

    Update TestCalculadora.java
    Remoção de anotações desnecessarias.

--- src/test/java/com/mycompany/testecalculadora/TestCalculadora.java
+++ src/test/java/com/mycompany/testecalculadora/TestCalculadora.java
@@ -16,35 +16,24 @@ class TestCalculadora {
     @Test
     void testGetDiferenca() {
         Calculadora calculadora = new Calculadora(4.0, 2.0);
-//Define o valor esperado para a operação
         double retornoEsperado = 2.0;
-//Chama a operacao getDiferenca()
         double retornoFeito = calculadora.getDiferenca();
-//Premissa verifica se os valores são iguais
         assertEquals(retornoEsperado, retornoFeito, 0);
     }
 
     @Test
     void testGetProduto() {
-//Instancia um objeto da classe Calculadora
         Calculadora calculadora = new Calculadora(4.0, 2.0);
-//Define o valor esperado para a operação
         double retornoEsperado = 8.0;
-//Chama a operacao getProduto()
         double retornoFeito = calculadora.getProduto();
-//Premissa verifica se os valores são iguais
         assertEquals(retornoEsperado, retornoFeito, 0);
     }
 
     @Test
     void testGetQuociente() {
-//Instancia um objeto da classe Calculadora
         Calculadora calculadora = new Calculadora(4.0, 2.0);
-//Define o valor esperado para a operação
         double retornoEsperado = 2.0;
-//Chama a operacao getQuociente()
         double retornoFeito = calculadora.getQuociente();
-//Premissa verifica se os valores são iguais
         assertEquals(retornoEsperado, retornoFeito, 0);
     }
 }
